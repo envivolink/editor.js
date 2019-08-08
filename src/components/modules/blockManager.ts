c305b78da7000ab631066d92dee16d9cc0b864bc
Federico Paredes <federico.paredes@2w.de>
2019-08-08 10:19:11 +0200

    build

--- src/components/modules/blockManager.ts
+++ src/components/modules/blockManager.ts
@@ -597,11 +597,11 @@ export default class BlockManager extends Module {
   private bindEvents(block: Block): void {
     const {BlockEvents, Listeners} = this.Editor;
 
-    Listeners.on(block.holder, 'keydown', (event) => BlockEvents.keydown(event as KeyboardEvent), true);
+    /*Listeners.on(block.holder, 'keydown', (event) => BlockEvents.keydown(event as KeyboardEvent), true);
     Listeners.on(block.holder, 'mouseup', (event) => BlockEvents.mouseUp(event));
     Listeners.on(block.holder, 'mousedown', (event: MouseEvent) => BlockEvents.mouseDown(event));
     Listeners.on(block.holder, 'keyup', (event) => BlockEvents.keyup(event));
     Listeners.on(block.holder, 'dragover', (event) => BlockEvents.dragOver(event as DragEvent));
-    Listeners.on(block.holder, 'dragleave', (event) => BlockEvents.dragLeave(event as DragEvent));
+    Listeners.on(block.holder, 'dragleave', (event) => BlockEvents.dragLeave(event as DragEvent));*/
   }
 }
